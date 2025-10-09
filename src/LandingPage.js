07a86597ac7b5cf1256bdc97551d987af04c4788
aaron-vong <aaronong74@gmail.com>
2025-10-09 02:03:02 -0500

    Working on portfolio section

--- src/LandingPage.js
+++ src/LandingPage.js
@@ -39,14 +39,17 @@ function LandingPage() {
           <FaArrowDown className="arrow-icon"></FaArrowDown>
       </section>
       <section className="about">
-          <h1> about-me </h1>
+          <div className="about-header">
+            <h1> about-me </h1>
+            <div className="about-line"></div>
+          </div>
         <div className="about-content">
           <div className="about-text">
-          <p> Honestly, I’m still figuring it out. Right now, I’m focused on preparing for technical interviews, 
+          <p> Honestly, I’m still figuring it out. But right now, I’m focused on preparing for technical interviews, 
             learning new languages and frameworks, and just becoming a better programmer overall. 
             I recently picked up JavaScript, HTML, and CSS, and somehow managed to earn the trust of a 
             doctor to build their website. Since then, I’ve had more opportunities to build sites for others.
-            That's when I realized I really enjoyed solving problems and helping people. 
+            That's when I realized I really enjoyed solving problems and helping people. The sites can be found below in the portfolio section.
             </p>
 
             <p> <strong>Note:</strong> I gave up CS the first time. But here we are. Give something another try. You never know
@@ -57,7 +60,10 @@ function LandingPage() {
         </div>
       </section>
       <section className="portfolio">
-        <h1> portfolio</h1>
+        <div className="portfolio-header">
+           <h1>portfolio</h1>
+           <div className="about-line"></div>
+        </div>
         <div className="portfolio-content">
         </div>
       </section>
