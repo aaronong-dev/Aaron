77cfdb293ef0a85a17e18bd1799bc7093a500857
aaron-vong <aaronong74@gmail.com>
2026-01-08 17:54:23 -0600

    Update

--- src/LandingPage.js
+++ src/LandingPage.js
@@ -314,14 +314,11 @@ function LandingPage() {
             <p>October 2025</p>
             <a href="https://your-gracepoint-url.com" target="_blank" rel="noopener noreferrer" className="view-site-link">View Site</a>
           </div>
-          <div className={`portfolio-item development ${portfolioAnimationsStarted ? 'animate-in' : ''}`}>
+          <div className={`portfolio-item ${portfolioAnimationsStarted ? 'animate-in' : ''}`}>
             <img src="/Pictures/HaciendaResort.png" alt="Hacienda Del Sol Resort Website" />
-            <div className="development-overlay">
-              <div className="development-text">Currently In Development</div>
-            </div>
             <h3>Hacienda Del Sol Resort</h3>
             <p>November 2025</p>
-            <a href="https://theplaysage.com" target="_blank" rel="noopener noreferrer" className="view-site-link">View Site</a>
+            <a href="https://haciendadelsolresort.com" target="_blank" rel="noopener noreferrer" className="view-site-link">View Site</a>
           </div>
            <div className={`portfolio-item ${portfolioAnimationsStarted ? 'animate-in' : ''}`}>
             <div class="placeholder-box">
